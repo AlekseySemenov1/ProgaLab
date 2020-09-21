6d0931cfd9ef82552868239dd30e2ef06681a924
AlekseySemenov1 <71176575+AlekseySemenov1@users.noreply.github.com>
2020-09-21 12:26:19 +0500

    Update Lab0.java

--- Lab0.java
+++ Lab0.java
@@ -1,31 +1,54 @@
 public class Lab0
 {
-    public static void main(String[] args) 
+    public static float rand()
     {
-        short[] b = {19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2};
-        float[] x = new float[14];
-        for (int i=0;i<14;i++)
+        return (float) Math.random()*28-13;
+    }
+    
+    public static double form1(float a)
+    {
+        return Math.tan(a*(Math.PI+a))/3/4;
+    }
+    
+    public static double form2(float a)
+    {
+        return Math.pow (Math.E, Math.pow (1/(2*a), a))/ 0.5;
+    }
+    
+    public static double form3(float a)
+    {
+        return (Math.pow (Math.atan (1/Math.pow (Math.E, Math.abs(a))), 1/3) + 1/4) / Math.atan (0.1* (Math.pow ((a+0.5)/27, 2)));
+    }
+    
+    public static void main (String[] args) 
+    {
+        final int q=14, w=18;
+        short[] b = new short[w];
+        float[] x = new float[q];
+        for (int i=0;i<w;i++)
+            b[i] = (short) (w-i+1);
+        for (int i=0;i<q;i++)
         {
-            x[i] = (float) Math.random()*28-13;
+            x[i] = rand();
         }
-        double[][] d = new double[18][14];
-        for (int i=0;i<18;i++)
+        double[][] d = new double[w][q];
+        for (int i=0;i<w;i++)
         {
-            for (int j=0;j<14;j++)
+            for (int j=0;j<q;j++)
             {
                 if (b[i]==4)
-                   d[i][j]= Math.tan(x[j]*(Math.PI+x[j]))/3/4;
-                else if (b[i]==2 || b[i]==3 || b[i]==5 || b[i]==7 && b[i]==8 || b[i]==9 || b[i]==11 || b[i]==17)
+                   d[i][j]= form1(x[j]);
+                else if (b[i]==2 || b[i]==3 || b[i]==5 || b[i]==7 || b[i]==8 || b[i]==9 || b[i]==11 || b[i]==17)
                 {
-                    d[i][j] = Math.pow (Math.E, Math.pow (1/(2*x[j]), x[j]))/ 0.5;
+                    d[i][j] = form2(x[j]);
                 }
                 else
                 {
-                    d[i][j] = (Math.pow (Math.atan (1/Math.pow (Math.E, Math.abs(x[j]))), 1/3) + 1/4) / Math.atan (0.1* (Math.pow ((x[j]+0.5)/27, 2)));
+                    d[i][j] = form3(x[j]);
                 }
                 System.out.printf("%.4f  ", d[i][j]);
             }
 	    System.out.print("\n");
         }
-  }
+    }
 }
